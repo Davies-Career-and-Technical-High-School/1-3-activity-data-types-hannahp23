38163e1877cf71eb0ff9f20202a6545be945e456
Hannah Pacheco <hannah.pacheco@daviestech.org>
2020-10-02 15:26:43 +0000

    done

--- Question1.java
+++ Question1.java
@@ -1,9 +1,21 @@
-
+import java.util.Scanner;
 
 class Question1 {
     public static void main(String[] args) {
 
-        /* Write your code here */
+ Scanner scan = new Scanner(System.in);
 
+
+  double num1 = scan.nextDouble();
+  double num2 = scan.nextDouble();
+
+ System.out.println(num1);
+
+ System.out.println(num2);
+
+
+ System.out.println(num2);
+
+  System.out.println(num1);
     }
 }

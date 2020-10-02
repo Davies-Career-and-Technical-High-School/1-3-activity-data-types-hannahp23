123808625693b65ec98ddf31c99795e5ec59bf1a
Hannah Pacheco <hannah.pacheco@daviestech.org>
2020-10-02 23:20:52 +0000

    done

--- Question2.java
+++ Question2.java
@@ -1,9 +1,23 @@
-
-
+import java.util.Scanner;
 class Question2 {
     public static void main(String[] args) {
 
-        /* Write your code here */
+    Scanner scan = new Scanner(System.in);
+
+    String name;
+    System.out.println("Hi there.What is your name?" + name );
+    
+    name= scan.nextLine();
+
+    System.out.println("Hi" + name + "How old are you?");
+
+    int age; 
+
+    age= scan.nextInt();
+
+    System.out.println(name +"is" + age + "years old");
+
+
 
     }
 }
